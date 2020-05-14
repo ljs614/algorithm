8c1c86a462d1295099556b90f11fcabfa109439e
jeongseop.lee <jeongseop.lee@wizontech.com>
2020-05-14 15:16:16 +0900

    bubble sort

--- sort/sort.go
+++ sort/sort.go
@@ -1,6 +1,6 @@
 package sort
 
-//SelectionSort select min(max) item
+//SelectionSort ...
 func SelectionSort(list []int) {
 	for i := range list {
 		minIdx := i
@@ -12,4 +12,15 @@ func SelectionSort(list []int) {
 		list[i], list[minIdx] = list[minIdx], list[i]
 	}
 
+}
+
+//BubbleSort ...
+func BubbleSort(list []int) {
+	for i := range list {
+		for j := 0; j < len(list)-1-i; j++ {
+			if list[j] > list[j+1] {
+				list[j], list[j+1] = list[j+1], list[j]
+			}
+		}
+	}
 }
