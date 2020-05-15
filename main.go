d538ade13bb4c433054a06f2c4106eebed4ee0b4
jeongseop.lee <jeongseop.lee@wizontech.com>
2020-05-15 11:31:53 +0900

    counting sort

--- main.go
+++ main.go
@@ -7,13 +7,18 @@ import (
 )
 
 func main() {
-	list := []int{1, 10, 5, 8, 7, 6, 4, 3, 2, 9}
+	// list := []int{1, 10, 5, 8, 7, 6, 4, 3, 2, 9}
 	// sort.SelectionSort(list)
 	// sort.BubbleSort(list)
 	// sort.InsertionSort(list)
 	// sort.QuickSort(list, 0, len(list)-1)
 	// sort.QuickSortDesc(list, 0, len(list)-1)
 	// sort.MergeSort(list, 0, len(list)-1)
-	sort.HeapSort(list)
-	fmt.Println(list)
+	// sort.HeapSort(list)
+	cList := []int{
+		1, 3, 2, 4, 3, 2, 5, 3, 1, 2, 3, 4, 4, 3, 5, 1, 2, 3, 5, 2, 3, 1, 4, 3, 5, 1, 2, 1, 1, 2,
+	}
+	count := make([]int, 5)
+	sort.CountingSort(cList, count)
+	fmt.Println(cList)
 }
--- sort/sort.go
+++ sort/sort.go
@@ -169,3 +169,19 @@ func HeapSort(list []int) {
 		}
 	}
 }
+
+//CountingSort ...
+func CountingSort(list, count []int) {
+	for _, val := range list {
+		count[val-1]++
+	}
+	index := 0
+	for i, val := range count {
+		if val != 0 {
+			for j := 0; j < val; j++ {
+				list[index] = i + 1
+				index++
+			}
+		}
+	}
+}
